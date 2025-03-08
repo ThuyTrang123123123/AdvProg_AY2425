e3d9fae6f3271b3aaa6e2d81a7300e3738446297
Thuỳ Tranng <thuytranguete@gmail.com>
2025-03-08 08:28:06 +0700

    Update guessit.cpp

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -14,7 +14,9 @@ using namespace std;
 ***/
 int generateRandomNumber() {
     // TODO: Return the random number in range 1 to 100
-    return 100;
+    int x;
+    x = 1 + rand() % 100;
+    return x;
 }
 
 
@@ -26,8 +28,11 @@ int generateRandomNumber() {
 ***/
 int getPlayerGuess() {
     // TODO: Ask the player guest and return the player's number
-
-    return 1;
+    int x;
+    cout << "Doan la ";
+    cin >> x;
+    cout << endl;
+    return x;
 }
 
 
@@ -45,9 +50,19 @@ string getAnswer(int number, int randomNumber) {
               If number is lower than randomNumber, the answer is "Your number is lower."
               If number is equal randomNumber, the answer is "Congratulation! You win."
     ***/
-    string answer;
-
-    return answer;
+    string s;
+    if (number > randomNumber)
+    {
+        s = "Your number is higher.";
+    }
+    else if (number < randomNumber)
+    {
+        s = "Your number is lower.";
+    }
+    else{
+        s = "Congratulation! You win.";
+    }
+    return s;
 }
 
 
@@ -59,7 +74,10 @@ string getAnswer(int number, int randomNumber) {
 ***/
 bool checkSuccess(string answer) {
     // TODO: return the result after checking that player guessed right or wrong
-    
+    if (answer != "Congratulation! You win.")
+    {
+        return false;
+    }
     return true;
 }
 
@@ -73,7 +91,10 @@ bool checkSuccess(string answer) {
 bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
     bool result = false;
-
+    if (isContinued == 'Y')
+    {
+        result = true;
+    }
     return result;
 }
 
@@ -87,7 +108,9 @@ bool checkContinuePlaying(char isContinued) {
 char getPlayerOpinion() {
     // TODO: Ask the player about continue playing and return the player's choice
     char isContinued;
-
+    cout << "Co tiep tuc khong ";
+    cin >> isContinued;
+    cout << endl;
     return isContinued;
 }
 
